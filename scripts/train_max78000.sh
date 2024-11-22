85f333df291daeb63df5e0014f0d3e2dd75d1832
Keane Kiechel Tumanda <keaneadnamut@gmail.com>
2024-11-22 08:06:29 +0800

    Added custom dataset

--- scripts/train_max78000.sh
+++ scripts/train_max78000.sh
@@ -4,4 +4,7 @@
 # python train.py --epochs 7 --optimizer Adam --lr 0.001 --wd 0 --deterministic --compress policies/schedule-catsdogs.yaml --qat-policy policies/qat_policy_cd.yaml --model ai85cdnet --dataset cats_vs_dogs --confusion --param-hist --embedding --device MAX78000 "$@" --enable-tensorboard
 
 # KWS20
-python train.py --epochs 7 --optimizer Adam --lr 0.001 --wd 0 --deterministic --compress policies/schedule_kws20.yaml --model ai85kws20net --dataset KWS --confusion --device MAX78000 "$@" --enable-tensorboard
+# python train.py --epochs 7 --optimizer Adam --lr 0.001 --wd 0 --deterministic --compress policies/schedule_kws20.yaml --model ai85net20 --dataset KWS --confusion --device MAX78000 "$@"
+
+# Custom Dataset
+python train.py --epochs 15 --optimizer Adam --lr 0.001 --wd 0 --deterministic --compress policies/schedule-alzheimers.yaml --qat-policy policies/qat_policy_cd.yaml --model ai85cdnet --dataset my_custom_dataset --confusion --param-hist --embedding --device MAX78000 "$@" --enable-tensorboard
